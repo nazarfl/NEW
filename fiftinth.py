64b5f2ccaea56738d810c793f2ba51f262092b4f
nazarfl <nazarfl88@gmail.com>
2022-05-25 20:19:41 +0300

    Initial commit.

---
+++ fiftinth.py
@@ -0,0 +1,11 @@
+from helper import print_bord, try_to_move, is_board_completed
+
+bord = [15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 1, 2, '_']
+
+while not is_board_completed(bord):
+    print(print_bord(bord))
+    choice = int(input("Enter number to replace>"))
+    bord = try_to_move(bord, choice)
+
+print("YOU WIN!!!")
+print(print_bord(bord))
